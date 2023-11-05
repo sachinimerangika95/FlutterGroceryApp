66670eeafcb1f256d30a0fb8c9a94ae1d94e80ee
Sudesh Nimesha <sudeshnt93@gmail.com>
2023-11-05 09:32:22 +0700

    order confirmation page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:groceryapp/auth.dart';
-import 'package:groceryapp/pages/home_page.dart';
+import 'package:groceryapp/pages/order_confirmation.dart';
 import 'package:provider/provider.dart';
 
 import '../model/cart_model.dart';
@@ -145,7 +145,7 @@ class CartPage extends StatelessWidget {
                                           context,
                                           MaterialPageRoute(
                                             builder: (context) {
-                                              return const HomePage();
+                                              return OrderConfirmationPage();
                                             },
                                           ),
                                         )
---
+++ lib/pages/order_confirmation.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:groceryapp/pages/home_page.dart';
+
+void main() {
+  runApp(OrderConfirmationPage());
+}
+
+class OrderConfirmationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Icon(
+                Icons.check_circle_outline,
+                color: Colors.green,
+                size: 100.0,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  'Your order has been placed successfully!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              ElevatedButton(
+                child: Text('Continue Shopping'),
+                onPressed: () {
+                  // Navigate back to home page
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return HomePage();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
